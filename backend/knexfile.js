27f1cb25af3eeec7ccd46070f7396dc292ec310c
Brandon Day <brandontday7@gmail.com>
2017-10-19 19:05:34 +0000

    grammar fix in knexfile

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -12,7 +12,7 @@ module.exports = {
       database : process.env.DB_NAME,
       port     : process.env.DB_PORT,
       //ssl      : process.env.DB_PORT
-    }
+    },
     migrations: {
       directory: './db/migrations',
       tableName: 'migrations'
