12c4223a85bae01773fd31249709f2f665a09110
Brandon Day <brandontday7@gmail.com>
2017-10-21 16:09:38 +0000

    writing db helper functions. about halfway done

--- backend/public/javascript/database-functions.js
+++ backend/public/javascript/database-functions.js
@@ -18,7 +18,7 @@ var knex = require("knex")(knexConfig);
 
 
 // USERS FUNCTIONS
-function newUser(displayName, spotifyID, imageURL) {
+function addUser(displayName, spotifyID, imageURL) {
   let newUser = {
     display_name: displayName,
     spotify_id: spotifyID,
@@ -30,73 +30,88 @@ function newUser(displayName, spotifyID, imageURL) {
   knex('users').insert(newUser)
     .then(() => {
       console.log(`${displayName} was added to the database!`)
-      return
     })
     .catch(() => {
       console.log(`There was an error adding ${displayName} to the database`)
     })
 }
 
+// newUser('Harry Potter', 'abcdefg', 'https://static.independent.co.uk/s3fs-public/styles/story_medium/public/thumbnails/image/2015/12/04/15/harry-potter-philosophers-stone.jpg')
+
+
 function removeUser(id) {
   knex('users').where('id', id).del()
     .then(() => {
       console.log(`${id} was removed from the database`)
-      return
     })
     .catch(() => {
       console.log(`There was an error removing ${id} from the database`)
     })
 }
 
-// newUser('Harry Potter', 'abcdefg', 'https://static.independent.co.uk/s3fs-public/styles/story_medium/public/thumbnails/image/2015/12/04/15/harry-potter-philosophers-stone.jpg')
 // removeUser('Harry Potter', 3)
+
+
+
+function getUserByID(id, callback) {
+  let user = {}
+  knex('users').where('id', id)
+    .then((val) => {
+      user = {
+        id: val[0].id,
+        display_name: val[0].display_name,
+        image_urls: val[0].image_urls,
+        following_array: val[0].following.following_array
+      }
+    })
+    .then(() => {
+      callback(user)
+    })
+    .catch(() => {
+      console.log(`There was an error retrieving ${id} from the database`)
+    })
+
+}
+
+// getUserByID(1, function(response) {
+//     console.log(response)
+// })
+
+
 
 
 
 // TRACK FUNCTIONS
 function addTrack(trackName, spotifyID, imageURLs, audioFeatures) {
   let newTrack = {
-  track_name: trackName,
-  spotify_id: spotifyID,
-  image_urls: {
-    large: imageURLs[0].url,
-    medium: imageURLs[1].url,
-    small: imageURLs[2].url,
-  },
-  danceability: audioFeatures.danceability,
-  energy: audioFeatures.energy,
-  key: audioFeatures.key,
-  loudness: audioFeatures.loudness,
-  mode: audioFeatures.mode,
-  speechiness: audioFeatures.speechiness,
-  acousticness: audioFeatures.acousticness,
-  instrumentalness: audioFeatures.instrumentalness,
-  liveness: audioFeatures.liveness,
-  valence: audioFeatures.valence,
-  tempo: audioFeatures.tempo
+    track_name: trackName,
+    spotify_id: spotifyID,
+    image_urls: {
+      large: imageURLs[0].url,
+      medium: imageURLs[1].url,
+      small: imageURLs[2].url,
+    },
+    danceability: audioFeatures.danceability,
+    energy: audioFeatures.energy,
+    key: audioFeatures.key,
+    loudness: audioFeatures.loudness,
+    mode: audioFeatures.mode,
+    speechiness: audioFeatures.speechiness,
+    acousticness: audioFeatures.acousticness,
+    instrumentalness: audioFeatures.instrumentalness,
+    liveness: audioFeatures.liveness,
+    valence: audioFeatures.valence,
+    tempo: audioFeatures.tempo
   }
 
   knex('tracks').insert(newTrack)
     .then(() => {
       console.log(`${trackName} was added to the database`)
-      return
     })
     .catch (() => {
       console.log(`There was an error adding ${trackName} to the database`)
     })
 
-}
-
-//hopefully this function will not be used in regular app usage, the more tracks the more data!
-function removeTrack(id) {
-  knex('tracks').where('id', id).del()
-    .then(() => {
-      console.log(`${id} was removed from the database`)
-      return
-    })
-    .catch(() => {
-      console.log(`There was an error removing ${id} from the database`)
-    })
 }
 
 // let track_name = "Hedwig's Theme"
@@ -120,7 +135,63 @@ function removeTrack(id) {
 //   tempo: 173.930
 // }
 // addTrack(track_name, spotify_id, images, features)
+
+
+//hopefully this function will not be used in regular app usage, the more tracks the more data!
+function removeTrack(id) {
+  knex('tracks').where('id', id).del()
+    .then(() => {
+      console.log(`${id} was removed from the database`)
+    })
+    .catch(() => {
+      console.log(`There was an error removing ${id} from the database`)
+    })
+}
+
 // removeTrack(6)
+
+
+
+
+function getTrackByID(id, callback) {
+  let track = {}
+  knex('tracks').where('id', id)
+    .then((val) => {
+      track = {
+        id: val[0].id,
+        track_name: val[0].track_name,
+        spotify_id: val[0].spotify_id,
+        image_urls: {
+          large: val[0].image_urls.large,
+          medium: val[0].image_urls.medium,
+          small: val[0].image_urls.small,
+        },
+        danceability: val[0].danceability,
+        energy: val[0].energy,
+        key: val[0].key,
+        loudness: val[0].loudness,
+        mode: val[0].mode,
+        speechiness: val[0].speechiness,
+        acousticness: val[0].acousticness,
+        instrumentalness: val[0].instrumentalness,
+        liveness: val[0].liveness,
+        valence: val[0].valence,
+        tempo: val[0].tempo
+      }
+    })
+    .then(() => {
+      callback(track)
+    })
+    .catch(() => {
+      console.log(`There was an error retrieving ${id} from the database`)
+    })
+
+}
+
+// getTrackByID(1, function(response) {
+//     console.log(response)
+// })
+
 
 
 
@@ -142,21 +213,9 @@ function addArtist(artistName, spotifyID, imageURLs, genresArray) {
   knex('artists').insert(newArtist)
     .then(() => {
       console.log(`${artistName} has beeen added to the database`)
-      return
     })
     .catch(() => {
       console.log(`There was an error adding ${artistName} to the database`)
-    })
-}
-
-function removeArtist(id) {
-  knex('artists').where('id', id).del()
-    .then(() => {
-      console.log(`${id} was removed from the database`)
-      return
-    })
-    .catch(() => {
-      console.log(`There was an error removing ${id} from the database`)
     })
 }
 
@@ -170,8 +229,79 @@ function removeArtist(id) {
 // let genres_array = [ "pop rap", "rap" ]
 // addArtist(artist_name, spotify_id, image_urls, genres_array)
 
+
+
+function removeArtist(id) {
+  knex('artists').where('id', id).del()
+    .then(() => {
+      console.log(`${id} was removed from the database`)
+    })
+    .catch(() => {
+      console.log(`There was an error removing ${id} from the database`)
+    })
+}
+
 // removeArtist(5)
 
+
+
+function getArtistByID(id, callback) {
+  let artist = {}
+  knex('artists').where('id', id)
+    .then((val) => {
+      artist = {
+        id: val[0].id,
+        artist_name: val[0].artist_name,
+        spotify_id: val[0].spotify_id,
+        image_urls: {
+          large: val[0].image_urls.large,
+          medium: val[0].image_urls.medium,
+          small: val[0].image_urls.small
+        },
+        genres: val[0].genres.genres_array
+      }
+    })
+    .then(() => {
+      callback(artist)
+    })
+    .catch(() => {
+      console.log(`There was an error retrieving ${id} from the database`)
+    })
+
+}
+
+// getArtistByID(1, function(response) {
+//     console.log(response)
+// })
+
+
+// SELECT * FROM tracks
+//   JOIN artist_tracks ON tracks.id = track_id
+//   JOIN artists ON artist_id = artists.id
+//   WHERE artists.id = 1
+//   ORDER BY track_name desc;
+
+function getArtistTracks(id, callback) {
+  knex.from('tracks')
+    .innerJoin('artist_tracks', 'track_id', 'tracks.id')
+    .innerJoin('artists', 'artist_id', 'artists.id')
+    .where('artists.id', id)
+    .orderBy('track_name', 'desc')
+    .then((val) => {
+      let tracks = []
+      val.forEach((track => {
+        tracks.push(track)
+      }))
+      callback(tracks)
+    })
+    .catch(() => {
+      console.log(`There was an error retrieving tracks by ${id} from the database`)
+    })
+}
+
+// getArtistTracks(1, function(response) {
+//   console.log(response)
+// })
 
 
 
@@ -187,31 +317,55 @@ function addPlaylist(playlistName, spotifyPlaylistID, spotifyOwnerID) {
   knex('playlists').insert(newPlaylist)
     .then(() => {
       console.log(`${playlistName} has beeen added to the database`)
-      return
     })
     .catch(() => {
       console.log(`There was an error adding ${playlistName} to the database`)
     })
 }
 
+// let playlist_name = "Playlist 1"
+// let spotify_playlist_id = "12345"
+// let spotify_owner_id = 1
+// addPlaylist(playlist_name, spotify_playlist_id, spotify_owner_id)
+
+
 
 function removePlaylist(id) {
   knex('playlists').where('id', id).del()
     .then(() => {
       console.log(`${id} was removed from the database`)
-      return
     })
     .catch(() => {
       console.log(`There was an error removing ${id} from the database`)
     })
 }
 
-// let playlist_name = "Playlist 1"
-// let spotify_playlist_id = "12345"
-// let spotify_owner_id = 1
-
-// addPlaylist(playlist_name, spotify_playlist_id, spotify_owner_id)
 // removePlaylist(3)
+
+
+function getPlaylistByID(id, callback) {
+  let playlist = {}
+  knex('playlists').where('id', id)
+    .then((val) => {
+      playlist = {
+        id: val[0].id,
+        playlist_name: val[0].playlist_name,
+        spotify_playlist_id: val[0].spotify_playlist_id,
+        spotify_owner_id: val[0].spotify_owner_id,
+      }
+    })
+    .then(() => {
+      callback(playlist)
+    })
+    .catch(() => {
+      console.log(`There was an error retrieving ${id} from the database`)
+    })
+
+}
+
+// getPlaylistByID(1, function(response) {
+//     console.log(response)
+// })
 
 
 
