8b8cfaeb6d402f4c45fa398e07109a7bb5a3ddb7
vagrant <thecarloferrer@gmail.com>
2017-10-21 21:35:04 +0000

    Got a chart up in here (CurrentUser.jsx).

--- client/src/Charts/Pie.jsx
+++ client/src/Charts/Pie.jsx
@@ -42,4 +42,4 @@ class PieChart extends Component{
   }
 }
 
-export default Pie;
+export default PieChart;
--- client/src/Dashboard/CurrentUser.jsx
+++ client/src/Dashboard/CurrentUser.jsx
@@ -1,10 +1,50 @@
 import React, {Component} from 'react';
+import PieChart from '../Charts/Pie.jsx';
 
 class CurrentUser extends Component {
+
+constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['Toronto', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets:[{
+          label:'Population',
+          data:[617594, 181045, 153060, 106519, 105162, 95072],
+          backgroundColor:[
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
+            'rgba(153, 102, 255, 0.6)',
+            'rgba(255, 159, 64, 0.6)'
+          ],
+        }]
+      }
+    });
+  }
+
+
   render (){
     return(
         <div id="user-profile">
           Inside user-profile div
+        <PieChart
+        chartData={this.state.chartData}
+        text="Pie Chart in "
+        location="Lighthouse Labs"
+        legendPosition="top"/>
         </div>
       )
   }
