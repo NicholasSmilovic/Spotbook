4b2b1c924f22702b8c1120f52af95bc2a9740e47
vagrant <thecarloferrer@gmail.com>
2017-10-21 20:17:58 +0000

    Refactored for code legibility.

--- client/src/Dashboard/CurrentUser.jsx
+++ client/src/Dashboard/CurrentUser.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 // import Pie from './Charts/Pie.jsx';
 
-class CurrentUser extends Component{
+class CurrentUser extends Component {
   render (){
     return(
         <div id="user-profile">
