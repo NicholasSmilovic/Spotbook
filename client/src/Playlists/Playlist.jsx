45a21ddcd3e2d4248d02167fb9b8936716d60281
NicholasSmilovic <nicholas_smilovic@hotmail.com>
2017-10-22 01:38:15 +0000

    implemented sticky-react for scrolling playlists

--- client/src/Playlists/Playlist.jsx
+++ client/src/Playlists/Playlist.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { StickyContainer, Sticky } from 'react-sticky';
 import Songs from './Songs.jsx';
 
 
@@ -23,13 +24,33 @@ class Playlist extends Component{
 
     return(
         <div>
-          <span>
-            <h1>{this.props.playlist.name}</h1>
-            <button className="btn btn-primary" onClick={this.toggleButton}>Show Tracks</button>
-            <button className="btn btn-success">EDIT??</button>
-            <button className="btn btn-danger">DELETE?</button>
+        <StickyContainer>
+          <Sticky >
+            {
+              ({
+                style
+              }) => {
+                return(
+                  <header style={style} className="playlist-header row">
+                    <div className='col-sm-6 text-center'>
+                      <h1>{this.props.playlist.name}</h1>
+                    </div>
+                    <div className='col-sm-2 playlist-header-column'>
+                      <button className="btn btn-primary playlist-header-button" onClick={this.toggleButton}>Show Tracks</button>
+                    </div>
+                    <div className='col-sm-2 playlist-header-column'>
+                      <button className="btn btn-success playlist-header-button">EDIT??</button>
+                    </div>
+                    <div className='col-sm-2 playlist-header-column'>
+                      <button className="btn btn-danger playlist-header-button">DELETE?</button>
+                    </div>
+                  </header>
+                  )
+              }
+            }
+          </Sticky>
             {renderSongs}
-          </span>
+        </StickyContainer>
           <hr/>
         </div>
       )
