194cc91953aa79aa0e12b987037f47298e236952
vagrant <thecarloferrer@gmail.com>
2017-10-23 22:36:25 +0000

    Bustin' outta this branch.

--- client/src/Dashboard/CurrentUser.jsx
+++ client/src/Dashboard/CurrentUser.jsx
@@ -27,6 +27,16 @@ class CurrentUser extends Component {
 
   getChartData(){
     // Ajax calls here
+    $.get('http://localhost:3001/')
+    .done(result => {
+
+      // this.setState({ tracks: result });
+      console.log(result);
+    } )
+    .fail( err => {
+      console.log(err);
+    })
+
     let chart = Prettiness(SampleData(), Palette().cool_10);
     this.setState({ chartData: chart.data });
   }
