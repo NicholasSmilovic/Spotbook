0458a00ab9820f4b051289e2a6a8e510eacd41a2
vagrant <thecarloferrer@gmail.com>
2017-10-23 19:09:16 +0000

    Changed border width of elements in a chart to 0.  This can be changed later.

--- client/src/Charts/Prettiness.jsx
+++ client/src/Charts/Prettiness.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 const Prettiness = (chartData, palette) => {
     let data = chartData;
     data.datasets[0]["borderColor"] = '#EEEEEE';
-    data.datasets[0]["borderWidth"] = '1';
+    data.datasets[0]["borderWidth"] = '';
     data.datasets[0]["backgroundColor"] = palette;
 
     return {
