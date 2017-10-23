fa56741a9dfc4ffc1fd6e42e84c7f80da9bad53d
vagrant <thecarloferrer@gmail.com>
2017-10-23 22:55:00 +0000

    Chippin' away at getting DB data to front-end, but this code is in no state to push.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  DataHelpers.userHelpers.getUserTopTracks();
+  // res.send();
+
 });
 
 module.exports = router;
