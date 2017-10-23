6e51ca1df24ec86442ebac5e230ec9312fdb79cb
vagrant <thecarloferrer@gmail.com>
2017-10-23 19:43:14 +0000

    Refactored chart generation so hard in CurrentUser.jsx.

--- client/src/Dashboard/CurrentUser.jsx
+++ client/src/Dashboard/CurrentUser.jsx
@@ -4,8 +4,9 @@ import UserMatchSidebar from './UserMatchSidebar.jsx';
 import UserProfile from './UserProfile.jsx';
 import Navbar from '../partials/Navbar.jsx'
 
-import Prettiness from '../Charts/Prettiness.jsx'
 import SampleData from '../Charts/SampleChartData.jsx'
+
+import Prettiness from '../Charts/Prettiness.jsx'
 import Palette from '../Charts/Palette.jsx'
 import BarChart from '../Charts/_Bar.jsx'
 
@@ -24,26 +25,16 @@ class CurrentUser extends Component {
     this.getChartData();
   }
 
-  componentDidMount(){
-    this.makeChartPretty();
-  }
-
   getChartData(){
     // Ajax calls here
-    this.setState({ chartData:SampleData() });
+    let chart = Prettiness(SampleData(), Palette().cool_10);
+    this.setState({ chartData: chart.data });
   }
-
-  makeChartPretty(){
-    // Make copy of state, change copy of state, then make state the edited version of itself.
-    let prettyData = Prettiness( this.state.chartData, Palette().cool_10 );
-    this.setState({ chartData: prettyData });
-  }
-
 
   handleClickElement = (event) => {
     if (event[0]) {
       let index = event[0]['_index'];
-      let label = this.state.chartData.data.labels[index];
+      let label = this.state.chartData.labels[index];
 
       alert(`INDEX: ${index} => ${label}`);
     }
@@ -51,12 +42,12 @@ class CurrentUser extends Component {
   // *** Charts Requires Stuff Above
 
   render (){
+          // <UserProfile />
+          // <UserMatchSidebar />
     return(
 
         <div>
 
-          <UserProfile />
-          <UserMatchSidebar />
 
           <BarChart
             chartData={this.state.chartData}
