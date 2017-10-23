104078e754edb7268daf677be997666fea3bf7a6
vagrant <thecarloferrer@gmail.com>
2017-10-23 00:10:27 +0000

    Refactored for colour scheme modularity.

--- client/src/Dashboard/CurrentUser.jsx
+++ client/src/Dashboard/CurrentUser.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import PieChart from '../Charts/Pie.jsx';
+// import PieChart from '../Charts/Pie.jsx';
 import BarChart from '../Charts/Bar.jsx';
 
 class CurrentUser extends Component {
@@ -7,7 +7,25 @@ class CurrentUser extends Component {
   constructor(){
     super();
     this.state = {
-      chartData:{}
+      chartData:{},
+      palette: {
+        cool: [
+        '#59F64B',
+        '#6200EA',
+        '#0CE486',
+        '#4E9CF5',
+        '#B2FF59',
+        '#9CF1E0',
+        ],
+        warm: [
+        '#AB2593',
+        '#EC192C',
+        '#ED30A0',
+        '#FE3A25',
+        '#C67B52',
+        '#FAE527',
+        ]
+      }
     }
   }
 
@@ -21,16 +39,9 @@ class CurrentUser extends Component {
       chartData:{
         labels: ['Carlo', 'Nich', 'Brandon', 'Bryce', 'Dummy_1', 'Dummy_2'],
         datasets:[{
-          label:'Points',
+          label:'Stuff',
           data:[50,30,70,100,110,20],
-          backgroundColor:[
-            '#59F64B',
-            '#6200EA',
-            '#0CE486',
-            '#4E9CF5',
-            '#B2FF59',
-            '#9CF1E0',
-          ],
+          backgroundColor:this.state.palette.warm,
         }]
       }
     });
@@ -41,7 +52,7 @@ class CurrentUser extends Component {
       let index = event[0]['_index'];
       let label = this.state.chartData.labels[index];
 
-      console.log(`INDEX: ${index} => ${label}`);
+      alert(`INDEX: ${index} => ${label}`);
     }
   }
 
@@ -53,12 +64,6 @@ class CurrentUser extends Component {
         <BarChart
         chartData={this.state.chartData}
         text="Bar Chart in "
-        location="Spotcheck"
-        handleClick={ event => this.handleClickElement(event) }/>
-
-        <PieChart
-        chartData={this.state.chartData}
-        text="Pie Chart in "
         location="Spotcheck"
         handleClick={ event => this.handleClickElement(event) }/>
 
