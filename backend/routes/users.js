a4a1e911788ea2dca4dd70e532379b7e30b3551e
vagrant <thecarloferrer@gmail.com>
2017-10-24 14:54:43 +0000

    A route can send stuff from backend to frontend via res.send().

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,8 +3,6 @@
 const express = require('express');
 const router = express.Router();
 
-
-
 // router.get('/getUserByID', function(req, res) {
 //   console.log('GET USER BY ID!!!');
 //   res.send();
@@ -16,7 +14,11 @@ module.exports = (DataHelpers) => {
 
   router.get('/getUserByID', (req, res) => {
     console.log('Hello from /users/getUserByID in /backend/routes/users.js');
-    // res.send();
+
+    res.send('Hello from /users/getUserByID in /backend/routes/users.js');
+
+
+
   });
 
   return router;
