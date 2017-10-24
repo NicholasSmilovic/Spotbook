3bba1c2c29009395804e01578c5ccc87f5e6e23a
vagrant <thecarloferrer@gmail.com>
2017-10-24 16:54:34 +0000

    Fixed bug that disallowed backend to run.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,34 @@
-var express = require('express');
-var router = express.Router();
+"use strict";
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  DataHelpers.userHelpers.getUserTopTracks();
-  // res.send();
 
-});
+const express = require('express');
+const router = express.Router();
 
-module.exports = router;
+
+// router.get('/getUserByID', function(req, res) {
+//   console.log('GET USER BY ID!!!');
+//   res.send();
+// });
+
+// module.exports = router;
+
+module.exports = (DataHelpers) => {
+
+  router.get('/getUserByID', (req, res) => {
+    console.log('Hello from /users/getUserByID in /backend/routes/users.js');
+
+    let user = DataHelpers.userHelpers.getUserByID('1');
+    console.log(user);
+    res.send(user);
+  });
+
+  router.get('/getUserTopTracks', (req, res) => {
+    // DataHelpers.userHelpers.getUserTopTracks()
+
+    // res.send();
+  });
+
+
+
+  return router;
+};
