28a440c28d942d4f07dd743a1f30903614c264a7
Bryce <brycewalterparker@gmail.com>
2017-10-24 14:37:56 +0000

    User page styling refactor

--- client/src/Dashboard/CurrentUser.jsx
+++ client/src/Dashboard/CurrentUser.jsx
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
 import UserMatchSidebar from './UserMatchSidebar.jsx';
 import UserProfile from './UserProfile.jsx';
-import Navbar from '../partials/Navbar.jsx'
+import UserBoxAnalytics from './UserBoxAnalytics.jsx';
 
 class CurrentUser extends Component {
   render (){
     return(
-        <div>
-          <UserProfile />
-          <UserMatchSidebar />
+      <div>
+
+        <UserProfile />
+
+        <div className='row'>
+          <div className='col-md-3'>
+          </div>
+          <div className='col-md-6 top-matches-text'>
+          <h1 style={{color: 'white'}}>Your Top Musical Matches</h1>
+          </div>
+          <div className='col-md-3'>
+          </div>
         </div>
+
+        <UserMatchSidebar />
+        <UserBoxAnalytics />
+      </div>
       )
   }
 }
--- client/src/Dashboard/UserMatch.jsx
+++ client/src/Dashboard/UserMatch.jsx
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 
 let pictureStyle = {borderRadius: '50%',
                     marginLeft: '2.5em',
-                    marginTop: '1em'}
+                    marginTop: '1em',
+                    display: 'block',
+                    margin: 'auto'}
 let displayNameStyle = {textAlign: 'center'}
 let matchPercentStyle = {textAlign: 'center', color: '#1ED760'}
 
@@ -10,47 +12,47 @@ class UserMatch extends Component{
   render (){
     return(
         <div>
-          <div className='userMatchProfile'>
+          <div className='user-match-profile'>
             <img style={pictureStyle}src='https://scontent.xx.fbcdn.net/v/t1.0-1/s200x200/13775793_10153610171132687_1785877468594627692_n.jpg?oh=1c88fcbb62a8d746c544b6bbed9b8f9e&oe=5A775134' />
             <h2 style={displayNameStyle}>His Excellency, John Gustavo IV, King of Spain and Andorra, Ruler of the Mediterranean</h2>
           </div>
-          <div className='userMatchPercentage'>
+          <div className='user-match-percentage'>
             <h4 style={{paddingTop: '0.5em', textAlign:'center'}}> You are: </h4>
             <h3 style={matchPercentStyle}> 82% matched! </h3>
           </div>
           <hr></hr>
-          <div className='userMatchProfile'>
+          <div className='user-match-profile'>
             <img style={pictureStyle}src='https://scontent.xx.fbcdn.net/v/t1.0-1/s200x200/13775793_10153610171132687_1785877468594627692_n.jpg?oh=1c88fcbb62a8d746c544b6bbed9b8f9e&oe=5A775134' />
             <h2 style={displayNameStyle}>His Excellency, John Gustavo IV, King of Spain and Andorra, Ruler of the Mediterranean</h2>
           </div>
-          <div className='userMatchPercentage'>
+          <div className='user-match-percentage'>
             <h4 style={{paddingTop: '0.5em', textAlign:'center'}}> You are: </h4>
             <h3 style={matchPercentStyle}> 82% matched! </h3>
           </div>
           <hr></hr>
-          <div className='userMatchProfile'>
+          <div className='user-match-profile'>
             <img style={pictureStyle}src='https://scontent.xx.fbcdn.net/v/t1.0-1/s200x200/13775793_10153610171132687_1785877468594627692_n.jpg?oh=1c88fcbb62a8d746c544b6bbed9b8f9e&oe=5A775134' />
             <h2 style={displayNameStyle}>His Excellency, John Gustavo IV, King of Spain and Andorra, Ruler of the Mediterranean</h2>
           </div>
-          <div className='userMatchPercentage'>
+          <div className='user-match-percentage'>
             <h4 style={{paddingTop: '0.5em', textAlign:'center'}}> You are: </h4>
             <h3 style={matchPercentStyle}> 82% matched! </h3>
           </div>
           <hr></hr>
-          <div className='userMatchProfile'>
+          <div className='user-match-profile'>
             <img style={pictureStyle}src='https://scontent.xx.fbcdn.net/v/t1.0-1/s200x200/13775793_10153610171132687_1785877468594627692_n.jpg?oh=1c88fcbb62a8d746c544b6bbed9b8f9e&oe=5A775134' />
             <h2 style={displayNameStyle}>His Excellency, John Gustavo IV, King of Spain and Andorra, Ruler of the Mediterranean</h2>
           </div>
-          <div className='userMatchPercentage'>
+          <div className='user-match-percentage'>
             <h4 style={{paddingTop: '0.5em', textAlign:'center'}}> You are: </h4>
             <h3 style={matchPercentStyle}> 82% matched! </h3>
           </div>
           <hr></hr>
-          <div className='userMatchProfile'>
+          <div className='user-match-profile'>
             <img style={pictureStyle}src='https://scontent.xx.fbcdn.net/v/t1.0-1/s200x200/13775793_10153610171132687_1785877468594627692_n.jpg?oh=1c88fcbb62a8d746c544b6bbed9b8f9e&oe=5A775134' />
             <h2 style={displayNameStyle}>His Excellency, John Gustavo IV, King of Spain and Andorra, Ruler of the Mediterranean</h2>
           </div>
-          <div className='userMatchPercentage'>
+          <div className='user-match-percentage'>
             <h4 style={{paddingTop: '0.5em', textAlign:'center'}}> You are: </h4>
             <h3 style={matchPercentStyle}> 82% matched! </h3>
           </div>
