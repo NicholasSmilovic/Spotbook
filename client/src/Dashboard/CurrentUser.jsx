388859bba04622694bd78d6a573af4506dc9fb27
Bryce <brycewalterparker@gmail.com>
2017-10-25 15:51:39 +0000

    added new route and local storage for access token

--- client/src/Dashboard/CurrentUser.jsx
+++ client/src/Dashboard/CurrentUser.jsx
@@ -10,6 +10,8 @@ import Prettiness from '../Charts/Prettiness.jsx'
 import Palette from '../Charts/Palette.jsx'
 import BarChart from '../Charts/_Bar.jsx'
 
+import { parse } from 'query-string'
+
 
 class CurrentUser extends Component {
 
@@ -21,6 +23,13 @@ class CurrentUser extends Component {
       currentUser:{}
     }
   }
+
+  // componentDidMount() {
+  //   let { access_token } = parse(location.search)
+
+  //   if (access_token) localStorage.access_token = access_token
+
+  // }
 
   componentWillMount(){
     this.setCurrentUser();
