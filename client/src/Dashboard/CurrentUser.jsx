b49c24985827ca7da818dbe51c4b4b499a9aa9ec
Bryce <brycewalterparker@gmail.com>
2017-10-27 16:44:28 +0000

    audio track features saved in user state

--- client/src/Dashboard/CurrentUser.jsx
+++ client/src/Dashboard/CurrentUser.jsx
@@ -13,6 +13,7 @@ import BarChart from '../Charts/_Bar.jsx'
 
 import TopArtistInsight from '../Insights/_TopArtist.jsx'
 import { parse } from 'query-string'
+import compatibilityFunctions from '../../../user-compatibility.js'
 
 
 
@@ -24,8 +25,52 @@ class CurrentUser extends Component {
       chartData:{},
       topTracks:[],
       topArtists:[],
+      userAudioTrackFeatures: {},
       insightData:'Stuff',
       compatibleUsers: []
+    }
+
+    this.getUserTrackAudioFeatures = (topTracks) => {
+      let danceability = 0
+      let energy = 0
+      let key = 0
+      let loudness = 0
+      let mode = 0
+      let speechiness = 0
+      let acousticness = 0
+      let instrumentalness = 0
+      let liveness = 0
+      let valence = 0
+      let tempo = 0
+
+      for (let track in topTracks) {
+        // console.log(topTracks[track])
+        danceability += topTracks[track].danceability
+        energy += topTracks[track].energy
+        key += topTracks[track].key
+        loudness += topTracks[track].loudness
+        mode += topTracks[track].mode
+        speechiness += topTracks[track].speechiness
+        acousticness += topTracks[track].acousticness
+        instrumentalness += topTracks[track].instrumentalness
+        liveness += topTracks[track].liveness
+        valence += topTracks[track].valence
+        tempo += topTracks[track].tempo
+      }
+
+      let userAudioTrackFeaturesAverages = { danceability: danceability/topTracks.length,
+                                             energy: energy/topTracks.length,
+                                             key: key/topTracks.length,
+                                             loudness: loudness/topTracks.length,
+                                             mode: mode/topTracks.length,
+                                             speechiness: speechiness/topTracks.length,
+                                             acousticness: acousticness/topTracks.length,
+                                             instrumentalness: instrumentalness/topTracks.length,
+                                             liveness: liveness/topTracks.length,
+                                             valence: valence/topTracks.length,
+                                             tempo: tempo/topTracks.length
+                                            }
+      return userAudioTrackFeaturesAverages
     }
   }
 
@@ -40,7 +85,10 @@ class CurrentUser extends Component {
       console.log("We got it. The thing that you need immediately follows this sentence.")
       console.log(this.props.currentLocal);
       this.getUserTopAbsArtists()
-      // this.getUserTopTracks();
+      this.getUserTopTracks().then(() => {
+        return this.setState({userAudioTrackFeatures: this.getUserTrackAudioFeatures(this.state.topTracks)})
+      })
+
 // ***** ***** ***** ***** *****
       // this.testRoute();
 // ***** ***** ***** ***** *****
@@ -63,7 +111,7 @@ class CurrentUser extends Component {
 
   // Grab user's top tracks
   getUserTopTracks(){
-    $.get('http://localhost:3000/users/getUserTopTracks/'+this.props.currentLocal.id)
+    return $.get('http://localhost:3000/users/getUserTopTracks/'+this.props.currentLocal.id)
     .done( topTrackIDs => {
       for (let i = 0; i < topTrackIDs.length; i++) {
         $.get('http://localhost:3000/tracks/getTrackByID/'+topTrackIDs[i].id)
@@ -83,9 +131,23 @@ class CurrentUser extends Component {
   }
 
   getUserTopAbsArtists() {
-    $.get('http://localhost:3000/users/getUserTopAbsArtists/' + this.props.currentLocal.id)
-    .done(result => {
-      console.log(result)
+    return $.get('http://localhost:3000/users/getUserTopAbsArtists/' + this.props.currentLocal.id)
+    .done(absArtistIDs => {
+      // console.log(absArtistIDs)
+      for(let i = 0; i < absArtistIDs.length; i++) {
+        $.get('http://localhost:3000/absArtists/getAbsArtistByID/' + absArtistIDs[i].id)
+        .done(artist => {
+          let topArtists = this.state.topArtists
+          topArtists.push(artist)
+          this.setState({topArtists})
+        })
+        .fail(err => {
+          console.error(err)
+        })
+      }
+    })
+    .fail(err => {
+      console.error(err)
     })
   }
 
@@ -108,6 +170,7 @@ class CurrentUser extends Component {
   }
 
   render (){
+    console.log(compatibilityFunctions.userAudioTrackFeaturesAverages)
     let user_img = '#'
     let user_name = null;
 
