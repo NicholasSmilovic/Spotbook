32e3483e505406fbebb67760c71af7e478910976
Brandon Day <brandontday7@gmail.com>
2017-10-30 20:19:59 +0000

    fixed bug with uncommon tracks

--- client/src/Users/User.jsx
+++ client/src/Users/User.jsx
@@ -26,28 +26,26 @@ class User extends Component{
     let uncommonTracks = []
     uncommonTracks = this.loops(y, m, uncommonTracks)
     uncommonTracks = this.loops(m, y, uncommonTracks)
+    console.log('uncommonTracks',uncommonTracks)
     return uncommonTracks
   }
 
-  loops = (primary, secondary, uncommonTracks) => {
-    for (let i = 0; i < primary.length; i++) {
-      let common = false
-      for (let j = 0; j < secondary.length; j++) {
-        if (primary[i] === secondary[j]) {
-          common = true
-          break
-        }
-        if (secondary[j] > primary[i]) {
-          break
-        }
-      }
-      if (!common) {
-        uncommonTracks.push(primary[i])
+loops = (primary, secondary, uncommonTracks) => {
+  for (let i = 0; i < primary.length; i++) {
+    let common = false
+    for (let j = 0; j < secondary.length; j++) {
+      if (primary[i].id === secondary[j].id) {
+        common = true
+        break
       }
     }
-    return uncommonTracks
-
+    if (!common) {
+      uncommonTracks.push(primary[i])
+    }
   }
+  return uncommonTracks
+
+}
 
 
 normalize = (yourTopTracks, myTopTracks) => {
@@ -146,32 +144,43 @@ generatePlaylist = (trackIDs) => {
       .then((yourResponse) => {
         yourTopTracks = yourResponse
       })
-      .done(() => {
+      .then(() => {
         $.get(`http://localhost:3000/users/getUserTopTracks/${me.id}`)
           .then((myResponse) => {
             myTopTracks = myResponse
           })
           .then(() => {
-
             let playlistPool = this.findUncommonTracks(yourTopTracks, myTopTracks)
-            let playlistPoolNormalized = this.normalize(myTopTracks, yourTopTracks)
-            for (let i = 0; i < 20; i++) {
-              playlist.push(playlistPool[Math.floor(Math.random()*Math.random()*(playlistPoolNormalized.length - 1))])
-            }
+            if (playlistPool.length) {
+              let playlistPoolNormalized = this.normalize(myTopTracks, yourTopTracks)
+              for (let i = 0; i < 20; i++) {
+                playlist.push(playlistPool[Math.floor(Math.random()*Math.random()*(playlistPoolNormalized.length - 1))])
+              }
 
-            playlist = this.removeDuplicates(playlist)
-            return playlist
+              playlist = this.removeDuplicates(playlist)
+              return playlist
+            } else {
+              return 0
+            }
           })
           .then((response) => {
-            return Promise.all(response.map(track => {
-              return $.get(`http://localhost:3000/tracks/getTrackByID/${track.id}`)
-            }))
+            if (response !== 0) {
+              return Promise.all(response.map(track => {
+                return $.get(`http://localhost:3000/tracks/getTrackByID/${track.id}`)
+              }))
+            } else {
+              return 0
+            }
           })
           .then((response) => {
-            response.forEach(track => {
-              playlistSpotifyIDs.push(track.spotify_id)
-            })
-            this.generatePlaylist(playlistSpotifyIDs)
+            if (response !== 0) {
+              response.forEach(track => {
+                playlistSpotifyIDs.push(track.spotify_id)
+              })
+              this.generatePlaylist(playlistSpotifyIDs)
+            } else {
+              console.log('No playlist available!')
+            }
           })
       })
   }
@@ -184,18 +193,38 @@ generatePlaylist = (trackIDs) => {
   }
 
 
-  render (){
+  render () {
+    let isLoaded = this.state.user
+    let displayName = null
+    let explanation = null
+
+    if (isLoaded) {
+      displayName = <h1>{this.state.user.display_name}</h1>
+      explanation = <div className='w-100 row align-items-center'>
+        <div className="col u-complete-me-text">Generate a playlist based on yours and {this.state.user.display_name}'s top tracks!</div>
+      </div>
+    } else {
+      let name = 'Lando Calorisian'
+      displayName = name
+      explanation = <div className='col-md-12 u-complete-me-text'>
+        <p>Generate a playlist based on yours and {name}'s top tracks!</p>
+      </div>
+    }
+
     return(
-        <div>
-          <div className='row'>
-            <div className='col-md-12 user-profile'>
-              <img src={this.state.user.image_urls.image} />
-              <h1>{this.state.user.display_name}</h1>
-              <button className="col-xl-2 text-center btn btn-primary u-complete-me" onClick={this.uCompleteMe}>U Complete Me Playlist</button>
+      <div>
+        <div className='row'>
+          <div className='col-md-12 user-profile'>
+            <img className='user-profile' src={this.state.user.image_urls.image} />
+            {displayName}
+            <div className='w-100 row align-items-center'>
+              <button className="text-center btn btn-primary u-complete-me" onClick={this.uCompleteMe}>U Complete Me Playlist</button>
             </div>
+            {explanation}
           </div>
         </div>
-      )
+      </div>
+    )
   }
 }
 
