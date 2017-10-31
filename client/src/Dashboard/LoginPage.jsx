ef8b4fc0f9e119dedba330b8df5a5b2615488770
Bryce <brycewalterparker@gmail.com>
2017-10-31 19:55:50 +0000

    updated landing page

--- client/src/Dashboard/LoginPage.jsx
+++ client/src/Dashboard/LoginPage.jsx
@@ -10,13 +10,6 @@ class LoginPage extends Component{
           <div className="col-md-4 col-sm-4 col-xs-2"></div>
             <div className="col-md-4 col-sm-4 col-xs-8">
                <h1 className='app-name'>TUNER.</h1>
-            </div>
-          <div className="col-md-4 col-sm-4 col-xs-2"></div>
-        </div>
-
-        <div className="row tag-div">
-          <div className="col-md-4 col-sm-4 col-xs-2"></div>
-            <div className="col-md-4 col-sm-4 col-xs-8">
                <p className='tagline'>Dial in your Spotify experience.</p>
             </div>
           <div className="col-md-4 col-sm-4 col-xs-2"></div>
